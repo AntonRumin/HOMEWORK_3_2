f27e5c8e376d4b05959f8f1c27de43703d08c1f9
Anton_R <anton.spb.ru@gmail.com>
2022-12-23 23:21:17 +0300

    Add task #010 Second Digit of Number

---
+++ task010_SecondDigit/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Нахождение второй цифры трехзначного числа");
+
+Console.SetCursorPosition(10,3);
+
+Console.Write ("Введите трёхзначное число, N = ");
+
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int num = Math.Abs(n);
+
+Console.SetCursorPosition(10,6);
+
+if (num/100 >10 || num/100 < 1)
+{
+    Console.Write ("Введенное число не является трёхзначным");
+}
+else
+{
+    Console.Write ("Вторая цифра числа N, В = ");    
+    Console.WriteLine (num%100/10);
+}
+Console.SetCursorPosition(6,9);
+
+Console.Write("* * *");
+
