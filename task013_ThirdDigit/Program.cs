756f7f6a7ed2a45380f3c05562b54fb35038bfb5
Anton_R <anton.spb.ru@gmail.com>
2022-12-23 23:22:50 +0300

    Add task #15 Third Digit of Number

---
+++ task013_ThirdDigit/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.WriteLine("Программа выводит третью цифру числа (начиная отсчет слева)");
+
+Console.SetCursorPosition(10,2);
+
+Console.Write ("Введите целое число, N = ");
+
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+int check = 0, rest = 1;
+
+Console.SetCursorPosition(10,4);
+
+while (rest > 0)
+{
+    check = check + 1;
+
+    rest = num/(int)Math.Pow(10,check);
+}
+if (check < 3)
+{
+  Console.WriteLine ("Число содержит менее трех цифр"); 
+}
+else
+{
+Console.Write ("Третья цифра числа: ");
+Console.WriteLine(num/(int)Math.Pow(10,check-3)%10);
+}
+Console.SetCursorPosition(1,6);
+
+Console.Write("* * *");
